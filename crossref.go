3e2f357cd9a2130da76f82db438be18efed3bb87
Eiji Onchi <onchi.eiji@gmail.com>
2018-10-29 09:34:56 +0900

    Added documentation and more fields

--- crossref.go
+++ crossref.go
@@ -12,6 +12,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+// Client communicates with CrossRef API.
 type Client struct {
 	mailto  string
 	appname string
@@ -19,15 +20,19 @@ type Client struct {
 
 const api string = "https://api.crossref.org"
 
+// Debug checks the communication between the library and the API in detail.
 func Debug() {
 	log.SetLevel(log.DebugLevel)
 	log.Info("Changed to debugging mode")
 }
 
+// String implements the Stringer interface.
 func (c *Client) String() string {
 	return fmt.Sprintf("App: %s, MailTo: %s", c.appname, c.mailto)
 }
 
+// WorksJSON returns the raw JSON. Use this if you want to manually process the
+// JSON data.
 func (c *Client) WorksJSON(doi, sel string) ([]byte, error) {
 	log.WithFields(log.Fields{
 		"client": c,
@@ -57,11 +62,13 @@ func (c *Client) WorksJSON(doi, sel string) ([]byte, error) {
 	return raw, nil
 }
 
+// Contributor saves the name of the author in First Last name.
 type Contributor struct {
 	First string
 	Last  string
 }
 
+// Work is the processed JSON for direct access to the information.
 type Work struct {
 	Type       string
 	Titles     []string
@@ -70,10 +77,16 @@ type Work struct {
 	BookTitle  string
 	Authors    []Contributor
 	Date       string
-	DOI        string
+	Publisher  string
+	Editors    []Contributor
 	Issue      string
 	Volume     string
 	Pages      string
+	DOI        string
+	ISSNs      []string
+	ISSN       string
+	ISBNs      []string
+	ISBN       string
 }
 
 func (w *Work) populate(content map[string]interface{}) {
@@ -85,7 +98,25 @@ func (w *Work) populate(content map[string]interface{}) {
 	for _, v := range content["container-title"].([]interface{}) {
 		w.BookTitles = append(w.BookTitles, getS(v))
 	}
-	w.BookTitle = w.BookTitles[0]
+	if len(w.BookTitles) > 0 {
+		w.BookTitle = w.BookTitles[0]
+	}
+
+	issns, _ := content["ISSN"].([]interface{})
+	for _, v := range issns {
+		w.ISSNs = append(w.ISSNs, getS(v))
+	}
+	if len(w.ISSNs) > 0 {
+		w.ISSN = w.ISSNs[0]
+	}
+
+	isbns, _ := content["ISBN"].([]interface{})
+	for _, v := range isbns {
+		w.ISBNs = append(w.ISBNs, getS(v))
+	}
+	if len(w.ISBNs) > 0 {
+		w.ISBN = w.ISBNs[0]
+	}
 
 	authors, _ := content["author"].([]interface{})
 	for _, v := range authors {
@@ -98,11 +129,23 @@ func (w *Work) populate(content map[string]interface{}) {
 		w.Authors = append(w.Authors, co)
 	}
 
-	w.DOI = getS(content["DOI"])
+	editors, _ := content["editor"].([]interface{})
+	for _, v := range editors {
+		e := v.(map[string]interface{})
+		co := Contributor{
+			Last:  getS(e["family"]),
+			First: getS(e["given"]),
+		}
+
+		w.Editors = append(w.Editors, co)
+	}
+
 	w.Type = getS(content["type"])
+	w.Publisher = getS(content["publisher"])
 	w.Issue = getS(content["issue"])
 	w.Volume = getS(content["volume"])
 	w.Pages = getS(content["page"])
+	w.DOI = getS(content["DOI"])
 
 	date := content["issued"].(map[string]interface{})["date-parts"].([]interface{})[0].([]interface{})
 	for _, v := range date {
@@ -111,6 +154,7 @@ func (w *Work) populate(content map[string]interface{}) {
 	w.Date = strings.TrimRight(w.Date, "-")
 }
 
+// Works gets a processed metadata from a DOI.
 func (c *Client) Works(doi string) (*Work, error) {
 	js, err := c.WorksJSON(doi, "title,container-title,author,issued,DOI,type,issue,volume,page")
 	if err != nil {
@@ -130,6 +174,8 @@ func (c *Client) Works(doi string) (*Work, error) {
 	return &w, nil
 }
 
+// QueryJSON gets the raw JSON from CrossRef. Use this if you want to manually
+// manipulate the JSON data.
 func (c *Client) QueryJSON(search string) ([]byte, error) {
 	log.WithFields(log.Fields{
 		"client": c,
@@ -164,6 +210,7 @@ func (c *Client) QueryJSON(search string) ([]byte, error) {
 	return raw, nil
 }
 
+// Query returns a list of processed Work metadata from a search query.
 func (c *Client) Query(search string) ([]*Work, error) {
 	js, err := c.QueryJSON(search)
 	works := []*Work{}
@@ -198,6 +245,8 @@ func getI(i interface{}) int {
 	return int(n)
 }
 
+// NewClient returns a new crossref client with an attached mailto and app
+// name.
 func NewClient(appname, malito string) *Client {
 	c := Client{
 		mailto:  malito,
